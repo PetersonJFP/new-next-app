27a883dd74acade97199e88c2fa5982bf35c3ea5
Peterson Passos <peterson.jfp@gmail.com>
2021-05-16 09:16:50 -0300

    add eslint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import '../styles/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
