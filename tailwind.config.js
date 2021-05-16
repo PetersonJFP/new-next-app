a31ca5b73c52312dd42902e27ef4e70ebd209afd
Peterson Passos <peterson.jfp@gmail.com>
2021-05-16 06:49:37 -0300

    add  typescript and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: '#504689',
+          '50': '#D2CEE6',
+          '100': '#C2BDDE',
+          '200': '#A39CCC',
+          '300': '#847ABB',
+          '400': '#6458AA',
+          '500': '#504689',
+          '600': '#3C3567',
+          '700': '#292445',
+          '800': '#151224',
+          '900': '#010102'
+        },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
